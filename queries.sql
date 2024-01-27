33ac707d067c6ed0c493672590d7e682001b5973
Yulian Khan <90974611+ikhanter@users.noreply.github.com>
2024-01-27 11:26:51 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -17,3 +17,19 @@ order by income desc
 limit 10;
 
 
+-- Запрос для поиска продавцов, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам
+-- Складывает имя и фамилию в единое поле таблицы, через группировку считает для каждого значения такого поля среднюю выручку за сделку, округленную
+-- в меньшую сторону и затем сравнивает ее со средней выручкой за сделку по всем продажам, посчитанной в подзапросе. Далее сортирует в по рядке возрастания
+-- средней выручки
+select
+	concat(e.first_name, ' ', e.last_name) as name,
+	floor(avg(p.price * s.quantity)) as average_income
+from employees e
+inner join sales s on s.sales_person_id = e.employee_id 
+inner join products p on p.product_id = s.product_id 
+group by concat(e.first_name, ' ', e.last_name)
+having floor(avg(p.price * s.quantity)) < (
+	select avg(p.price * s.quantity)
+	from sales s inner join products p on s.product_id = p.product_id 
+)
+order by average_income;
